28b0cf2ce537b7dee6af2c21fb36f5b0cc8545ea
Javier de la Llave <jdelallalor@gmail.com>
2020-01-14 20:10:15 +0100

    le hemos añadido un metodo que quita espacios

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -13,9 +13,28 @@ public class PruebaGit {
 
     /**
      * @param args the command line arguments
+     * 
      */
+    
+    //Quita los espacios de un String
+    private String quita(String str){
+        String noEspacios = "";
+        for (int i = 0; i < str.length(); i++) {//guardamos str sin espacios
+            if (str.charAt(i) != ' ') {
+                noEspacios += str.charAt(i);
+            }
+        }
+        
+        return noEspacios;
+    }
+    
+    
     public static void main(String[] args) {
-        System.out.println("prueba git");
+        
+        //https://github.com/LeopoldoDeLaLlave/pruebGit.git
+        //he añadido un métodeo que quita espacios
+        
+        
     }
     
 }
